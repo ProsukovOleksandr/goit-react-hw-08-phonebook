8b2ebae746e7a90fe7ce1ff575dd6b574a0972ab
ProsukovOleksandr <122283728+ProsukovOleksandr@users.noreply.github.com>
2023-09-09 16:04:41 +0000

    Deploying to gh-pages from @ ProsukovOleksandr/goit-react-hw-08-phonebook@ec8fe1c4f07b22a5f77c2a4890852d08879c0b88 🚀

---
+++ static/js/805.37a8fe54.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{9805:function(e,r,t){t.r(r);t(2791);var a=t(1087),o=t(7751),n=t(184);r.default=function(){return(0,n.jsxs)("div",{children:["Error 404. Sorry, page wasn`t found."," ",(0,n.jsx)(a.rU,{to:o.ef,children:"Back to the Home page."})]})}}}]);
+//# sourceMappingURL=805.37a8fe54.chunk.js.map
